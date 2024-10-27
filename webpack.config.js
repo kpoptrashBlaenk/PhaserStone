36f595451876087dcacb8c1f46d6cff01c842635
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-27 17:26:41 +0100

    Project-Setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
