b6adf3d4a42b0d4fcaef48eb3ff398128c10878c
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-31 14:18:57 +0100

    trivial

--- src/gameObjects/card-keys.ts
+++ src/gameObjects/card-keys.ts
@@ -1,14 +1,14 @@
 export const CLASS_KEYS = Object.freeze({
-    NEUTRAL: 'NEUTRAL',
-  })
-  export type ClassKeys = keyof typeof CLASS_KEYS
-  
-  export const TYPE_KEYS = Object.freeze({
-    MINION: 'MINION',
-  })
-  export type TypeKeys = keyof typeof TYPE_KEYS
-  
-  export const RACE_KEYS = Object.freeze({
-    BEAST: 'BEAST',
-  })
-  export type RaceKeys = keyof typeof TYPE_KEYS
+  NEUTRAL: 'NEUTRAL',
+})
+export type ClassKeys = keyof typeof CLASS_KEYS
+
+export const TYPE_KEYS = Object.freeze({
+  MINION: 'MINION',
+})
+export type TypeKeys = keyof typeof TYPE_KEYS
+
+export const RACE_KEYS = Object.freeze({
+  BEAST: 'BEAST',
+})
+export type RaceKeys = keyof typeof TYPE_KEYS
