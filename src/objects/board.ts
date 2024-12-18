27cc05de47de4a110547894400feec7340db524f
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-12-18 12:42:59 +0100

    Add: Board

---
+++ src/objects/board.ts
@@ -0,0 +1,41 @@
+import { resizeContainer } from '../common/resize-container'
+import { AnimationManager } from '../utils/animation-manager'
+import { TARGET_KEYS, TargetKeys } from '../utils/keys'
+import { BOARD_CONFIG, CARD_CONFIG } from '../utils/visual-configs'
+import { Card } from './card'
+
+export class Board {
+  private $scene: Phaser.Scene
+  private $owner: TargetKeys
+  private $animationManager: AnimationManager
+  private $board: Card[]
+  private $boardContainer: Phaser.GameObjects.Container
+
+  constructor(scene: Phaser.Scene, owner: TargetKeys, animationManager: AnimationManager) {
+    this.$scene = scene
+    this.$owner = owner
+    this.$animationManager = animationManager
+    this.$board = []
+
+    this.$createContainer()
+    this.$resizeContainer()
+  }
+
+  /**
+   * Create empty board container
+   */
+  private $createContainer(): void {
+    this.$boardContainer = this.$scene.add.container()
+  }
+
+  /**
+   * Resize container and reposition it
+   */
+  private $resizeContainer(): void {
+    resizeContainer(
+      this.$boardContainer,
+      this.$scene.scale.width / 2 - Math.max(this.$boardContainer.width, CARD_CONFIG.SIZE.WIDTH) / 2,
+      BOARD_CONFIG.POSITION_Y[this.$owner]
+    )
+  }
+}
